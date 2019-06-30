3b0b9c4a7321955111ea7d9747b77f35e5ff94f7
ananduks2016 <49419628+ananduks2016@users.noreply.github.com>
2019-06-29 20:17:48 -0700

    Update translation.py

--- tg_bot/modules/translation.py
+++ tg_bot/modules/translation.py
@@ -52,7 +52,7 @@ __help__ = """
  - /t: while replying to a message, will reply with a grammar corrected version
 """
 
-__mod_name__ = "Translator"
+
 
 
 TRANSLATE_HANDLER = CommandHandler('t', translate)
